f1a1557ba3c6bca539a416c164535194906903ec
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-10-14 20:37:02 +0800

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="bg-white shadow-md h-16 flex justify-between items-stretch">
+    <RouterLink :to="{ name: 'home' }" class="inline-flex items-center h-full px-5">
+      Home
+    </RouterLink>
+    <div class="flex items-center gap-3">
+      <RouterLink
+        :to="{ name: 'byName' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-gray-400"
+      >
+        Search Meals
+      </RouterLink>
+      <RouterLink
+        :to="{ name: 'byLetter' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-gray-400"
+      >
+        Meals By Letter
+      </RouterLink>
+      <RouterLink
+        :to="{ name: 'byIngredient' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-gray-400"
+      >
+        Meals By Ingredients
+      </RouterLink>
+    </div>
+  </header>
+</template>
