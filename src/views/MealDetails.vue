d15bd02154c9336e43b94012819c50c9a7f321ae
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-10-14 20:37:30 +0800

    Create MealDetails.vue

---
+++ src/views/MealDetails.vue
@@ -0,0 +1,27 @@
+<script setup>
+    
+    import { ref } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+    const meal = ref({});
+
+    onMounted(async () => {
+  try {
+    const response = await axiosClient.get(`lookup.php?i=${route.params.id}`);
+    meal.value = response.data.meals[0];
+  } catch (error) {
+    console.error('Error fetching meal details:', error);
+  }
+});
+
+</script>
+
+<template>
+    <div>
+     <pre>{{ meal }}</pre>
+    </div>
+  </template>
+  
+  
+  
