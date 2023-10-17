26dac46084ae41492e0fa2adae3577db280dd5a6
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-10-17 22:21:06 +0800

    Button component change

--- src/components/YoutuButton.vue
+++ src/components/YoutuButton.vue
@@ -1,15 +1,14 @@
 <template>
-  <a
-    :href="href"
-    target="_blank"
-    class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors"
-  >
-    <slot>YouTube</slot>
+  <a :href="href" 
+  target="_blank" 
+  class="px-3 py-2 text-white rounded-lg border-2 shadow-md bg-red-500 hover:bg-red-600 transition-colors">
+  YouTube
   </a>
+
 </template>
 
 <script setup>
 const props = defineProps({
-  href: String
+href: String
 })
 </script>
