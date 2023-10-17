12335ac0d3ad88f13af216b4f9aef9e3980834f6
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-10-17 22:24:35 +0800

    add detail

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -23,9 +23,45 @@
 </script>
 
 <template>
-    <div>
-     <pre>{{ meal }}</pre>
-     //預覽meal變數中的變數資料
+    <div class="w-[800px] mx-auto p-8">
+        <h1 class="text-5x1 font-bold mb-5">{{ meal.strMeal }}</h1>
+        <img :src="meal.strMealThumb" :alt="meal.strMeal">
+        <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
+            <div>
+                <strong class="font-bold">Category:</strong>{{ meal.strCategory }}
+            </div>
+            <div>
+                <strong class="font-bold">Area:</strong>{{ meal.strArea }}
+            </div>
+            <div>
+                <strong class="font-bold">Tags:</strong>{{ meal.strTags }}
+            </div>
+        </div>
+                <div class="grid grid-cols-1 sm:grid-cols-2">
+                    <div>
+                        <h2 class="text-2x1 font-semibold mb-2">Ingredients</h2>
+                        <ul>
+                           <template v-for="(el,ind) of new Array(20)">
+                            <li v-if="meal[`strIngredient${ind + 1}`]">
+                                {{ind+1}}. {{ meal[`strIngredient${ind + 1}`] }}
+                            </li>
+                            </template>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 class="text-2x1 font-semibold mb-2">Measure</h2>
+                        <ul>
+                           <template v-for="(el,ind) of new Array(20)">
+                            <li v-if="meal[`strMeasure${ind + 1}`]">
+                                {{ind+1}}. {{ meal[`strMeasure${ind + 1}`] }}
+                            </li>
+                            </template>
+                        </ul>
+                    </div>
+                </div>
+    
+     <!--<pre>{{ meal }}</pre>-->
+    <!--預覽meal變數中的變數資料--> 
     </div>
   </template>
   
