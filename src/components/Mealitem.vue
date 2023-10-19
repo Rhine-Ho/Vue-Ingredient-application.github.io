cc6a09e1747a268b542987500e6623d84c6b4df9
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-10-19 10:58:07 +0800

    Update Mealitem.vue

--- src/components/Mealitem.vue
+++ src/components/Mealitem.vue
@@ -1,26 +1,5 @@
-<template>
-  <div class="bg-white shadow rounded-xl hover:scale-105 transition-all">
-    <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
-      <img
-        :src="meal.strMealThumb"
-        :alt="meal.strMeal"
-        class="rounded-t-xl w-full h-48 object-cover"
-      />
-    </router-link>
-    <div class="p-3">
-      <h3 class="font-bold">{{ meal.strMeal }}</h3>
-      <p class="mb-4">
-        {{ $filters.truncateWords(meal.strInstructions, 20) }}
-      </p>
-      <div class="flex items-center justify-between">
-        <YouTubeButton :href="meal.strYoutube" />
-      </div>
-    </div>
-  </div>
-</template>
-
 <script setup>
-import YouTubeButton from './YouTubeButton.vue'
+import YouTubeButton from './YoutuButton.vue';
 
 const { meal } = defineProps({
   meal: {
@@ -29,3 +8,31 @@ const { meal } = defineProps({
   }
 })
 </script>
+
+<template>
+  <div 
+    :key="meal.idMeal" 
+    class="bg-white shadow rounded-xl"
+    >
+    <img 
+    :src="meal.strMealThumb" 
+    :alt="meal.strMeal" 
+    class="rounded-t-xl w-full h-48 object-cover">
+
+      <div class="p-3">
+        <h3 class="font-bold">
+        {{ meal.strMeal }}</h3><!-- 顯示餐點名稱-->
+      <!--<pre>{{ meals }}</pre>-->
+        <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat est dignissimos numquam tempore consequuntur a aperiam, quibusdam eaque temporibus quod soluta nesciunt odit nobis, expedita tenetur unde porro aut nihil.</p>
+      <div class="flex items-center justify-between">
+        <YouTubeButton :href="meal.strYoutube">Youtube</YouTubeButton>
+        <RouterLink :to="{ name: 'mealDetail', params: {id:meal.idMeal}}" class="px-3 py-2 rounded-lg border-2 bg-slate-200 hover:bg-slate-50 shadow-sm hover:shadow-slate-600 transition-colors">
+          View
+        </RouterLink>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+
