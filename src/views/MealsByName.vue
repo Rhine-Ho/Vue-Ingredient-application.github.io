5bc4a0d6f55574c14f498287e55fab96bda0b5c2
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-10-19 10:53:20 +0800

    將SearchByletter路由元件移至component

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed, ref, watch } from 'vue';// 引入Vue 3相關的功能
 import store from '../stores';// 引入存儲庫（store）
-import YoutuButton from '../components/YoutuButton.vue';
+import Mealitem from '../components/Mealitem.vue';
 
 const keyword = ref('');// 創建一個可響應的參考（ref）用於存儲搜尋關鍵字
 const meals = computed(() => store.state.searchedMeals); // 使用computed來從存儲庫中獲取餐點資料
@@ -27,28 +27,10 @@ watch(() => store.state.searchedMeals, () => {
       @change="searchMeals"
     />  <!--當輸入框的值變化時觸發searchMeals函數-->
   </div>
-<div class="grid grid-cols-1 md:grid-cols-3 gap-3 p-6">
-  <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
-    <img 
-    :src="meal.strMealThumb" 
-    :alt="meal.strMeal" 
-    class="rounded-t-xl w-full h-48 object-cover">
-
-      <div class="p-3">
-        <h3 class="font-bold">
-        {{ meal.strMeal }}</h3><!-- 顯示餐點名稱-->
-      <!--<pre>{{ meals }}</pre>-->
-        <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat est dignissimos numquam tempore consequuntur a aperiam, quibusdam eaque temporibus quod soluta nesciunt odit nobis, expedita tenetur unde porro aut nihil.</p>
-      <div class="flex items-center justify-between">
-        <YoutuButton :href="meal.strYoutube"/>
-        <RouterLink :to="{ name: 'mealDetail', params: {id:meal.idMeal}}" class="px-3 py-2 rounded-lg border-2  bg-slate-200 hover:bg-slate-50  shadow-sm hover:shadow-slate-600    transition-colors">
-          View
-        </RouterLink>
-      </div>
-
-    </div>
+  
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-3 p-6">
+    <Mealitem v-for="meal of meals" :key="meal.idMeal" :meal="meal"></Mealitem>
   </div>
-</div>
 </template>
 
 
