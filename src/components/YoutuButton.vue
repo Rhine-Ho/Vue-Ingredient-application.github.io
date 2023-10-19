962813ded2718d40e2506e050cfe1b3aca869034
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-10-19 10:57:58 +0800

    Update YoutuButton.vue

--- src/components/YoutuButton.vue
+++ src/components/YoutuButton.vue
@@ -2,7 +2,7 @@
   <a :href="href" 
   target="_blank" 
   class="px-3 py-2 text-white rounded-lg border-2 shadow-md bg-red-500 hover:bg-red-600 transition-colors">
-  YouTube
+  <slot></slot>
   </a>
 
 </template>
