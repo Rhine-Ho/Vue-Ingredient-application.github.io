0767ad09d8ecfe7b6e92a7bbe9a1daf73be95347
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-11-12 11:35:20 +0800

    Update MealsByIngredient.vue
    add ingredient

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import{computed, onMounted, ref} from 'vue';
+import axiosClient from '../axiosClient';
+import store from '../stores';
+
+const ingredients = ref([]);
+
+onMounted(()=>{
+  axiosClient.get('list.php?i=list')
+  .then(({data}) =>{
+    ingredients.value = data.meals;
+  });
+});
+</script>
 
 <template>
-  <div>
-    <h1>in</h1>
+  <div class="p-8">
+    <div v-for="ingredient of ingredients" :key="ingredient.id" 
+    class="bg-white break-word p-3 mb-3 grid grid-cols-1 md:grid-cols-3 gap-3 shadow">
+      <h3 class="text-2xl font-bold">{{  ingredient.strIngredient }}</h3>
+      <p>{{ ingredient.strDescription }}</p>
+    </div>
   </div> 
 </template>
